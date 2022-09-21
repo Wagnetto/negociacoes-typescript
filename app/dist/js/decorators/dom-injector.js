5a6656a1014b6fcc7d9de0d3dfd68d5b7b3e6046
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2022-09-21 18:01:45 -0300

    Finishing the decorators

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -3,10 +3,13 @@ export function domInjector(seletor) {
         console.log(`Modificando prototype ${target.constructor.name}
         e adicionando getter para a propriedade ${propertyKey}
         `);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do  com o seletor 
-            ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do  com o seletor 
+              ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,21 +1,19 @@
-export function domInjector(seletor:string) {
-    return function(target: any, propertyKey: string){
-        console.log(`Modificando prototype ${target.constructor.name}
+export function domInjector(seletor: string) {
+  return function (target: any, propertyKey: string) {
+    console.log(`Modificando prototype ${target.constructor.name}
         e adicionando getter para a propriedade ${propertyKey}
         `);
-        const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do  com o seletor 
-            ${seletor} para injetar em ${propertyKey}`);
-            return elemento;
-        }
+    let elemento: HTMLElement;
+    const getter = function () {
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+        console.log(`buscando elemento do  com o seletor 
+              ${seletor} para injetar em ${propertyKey}`);
+      }
 
-        Object.defineProperty(
-            target, 
-            propertyKey, 
-            {get: getter}
-        );
+      return elemento;
+    };
 
-
-    }
+    Object.defineProperty(target, propertyKey, { get: getter });
+  };
 }
