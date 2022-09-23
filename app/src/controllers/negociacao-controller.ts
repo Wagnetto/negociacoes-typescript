793899f6043ff2e6d86602beef0d5cc73c791aae
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2022-09-23 15:52:46 -0300

    Adds Imprimivel interface

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -5,6 +5,7 @@ import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { NegociacoesService } from "../services/negociacoes-service.js";
+import { imprimir } from "../utils/imprimir.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
@@ -42,20 +43,21 @@ export class NegociacaoController {
       return;
     }
 
-    this.negociacoes.adiciona(negociacao);
+    this.negociacoes.adiciona(negociacao);    
+    imprimir(negociacao, this.negociacoes);
     this.limparFormulario();
     this.atualizaView();
   }
 
   public importaDados(): void {
     this.negociacoesService
-    .obterNegociacoesDoDia()    
-    .then(negociacoesDeHoje => {
-        for(let negociacao of negociacoesDeHoje) {
-            this.negociacoes.adiciona(negociacao);
+      .obterNegociacoesDoDia()
+      .then((negociacoesDeHoje) => {
+        for (let negociacao of negociacoesDeHoje) {
+          this.negociacoes.adiciona(negociacao);
         }
         this.negociacoesView.update(this.negociacoes);
-    })
+      });
   }
 
   private ehDiaUtil(data: Date) {
